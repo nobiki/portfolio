6b672828f3442d9df08532c17cf27db34e263ba4
Naoaki Obiki <obiki.wk@gmail.com>
2020-05-26 14:07:04 +0900

    gorm

---
+++ src/api/postgres.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/jinzhu/gorm"
+	// "github.com/bdwilliams/go-jsonify/jsonify"
+	_ "github.com/lib/pq"
+)
+
+type Prefecture struct {
+	Id int
+	Name string
+	NameKana string
+}
+var prefectures []Prefecture
+
+func main() {
+	r := gin.Default()
+	r.GET("/postgres/gorm", func(c *gin.Context) {
+		db, err := gorm.Open("postgres", "host=gorp-postgres user=user password=pass dbname=gorp sslmode=disable")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		rows := db.Find(&prefectures)
+		if err != nil {
+			panic(err)
+		}
+		defer rows.Close()
+
+		// output := jsonify.Jsonify(rows)
+		output := rows
+		fmt.Println(output)
+
+		c.JSON(200, output)
+	})
+	r.Run(":3001")
+}
+
+
