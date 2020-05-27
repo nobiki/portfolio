31e16b4f4f1cd7afafa8c87da1efb8d8d28a2034
Naoaki Obiki <obiki.wk@gmail.com>
2020-05-27 18:14:38 +0900

    add updated triger

--- postgres/docker-entrypoint-initdb.d/001_create_table.sql
+++ postgres/docker-entrypoint-initdb.d/001_create_table.sql
@@ -1,8 +1,9 @@
 begin;
 
 create domain created as timestamp default CURRENT_TIMESTAMP;
-create domain updated as timestamp default null;
+create domain updated as timestamp default CURRENT_TIMESTAMP;
 
+-- experiences
 create table experiences (
   id serial not null
   , description varchar(255)
@@ -17,5 +18,15 @@ comment on column experiences.description is 'description';
 comment on column experiences.text is 'text';
 comment on column experiences.created is 'created';
 comment on column experiences.updated is 'updated';
+
+create function set_update_time() returns opaque as '
+  begin
+    new.updated := ''now'';
+    return new;
+  end;
+' language 'plpgsql';
+
+create trigger update_trigger before update on experiences for each row
+  execute procedure set_update_time();
 
 commit;
