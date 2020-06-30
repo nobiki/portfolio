1a0a35d9d1d99e19857ae2709b4d10071b740125
Naoaki Obiki <obiki.wk@gmail.com>
2020-06-30 18:00:53 +0900

    Contact form

--- src/app/src/components/portfolio/Contact.js
+++ src/app/src/components/portfolio/Contact.js
@@ -2,6 +2,30 @@ import React, { Component } from "react";
 
 class Contact extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+    console.log(this.state);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
       <section id="contact">
@@ -13,59 +37,61 @@ class Contact extends Component {
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate
               </p>
             </div>
-            <div className="card mb-5 wow fadeInUp" data-wow-delay=".4s">
-              <div className="card-body p-5">
-                <div className="row">
-                  <div className="col-md-8">
-                    <form action="https://formspree.io/youremail@example.com" method="POST">
-                      <div className="row">
-                        <div className="col-md-6">
-                          <div className="md-form">
-                            <input className="form-control" id="name" type="text" name="name" required="required"/>
-                            <label for="name">Your name</label>
+            <form onSubmit={this.handleSubmit}>
+              <div className="card mb-5 wow fadeInUp" data-wow-delay=".4s">
+                <div className="card-body p-5">
+                  <div className="row">
+                    <div className="col-md-8">
+                      <form action="https://formspree.io/youremail@example.com" method="POST">
+                        <div className="row">
+                          <div className="col-md-6">
+                            <div className="md-form">
+                              <input className="form-control" id="name" type="text" name="name" required="required" onChange={this.handleChange}/>
+                              <label for="name">Your name</label>
+                            </div>
+                          </div>
+                          <div className="col-md-6">
+                            <div className="md-form">
+                              <input className="form-control" id="email" type="text" name="email" required="required" onChange={this.handleChange}/>
+                              <label for="email">Your email</label>
+                            </div>
                           </div>
                         </div>
-                        <div className="col-md-6">
-                          <div className="md-form">
-                            <input className="form-control" id="email" type="text" name="_replyto" required="required"/>
-                            <label for="email">Your email</label>
+                        <div className="row">
+                          <div className="col-md-12">
+                            <div className="md-form">
+                              <input className="form-control" id="subject" type="text" name="subject" required="required" onChange={this.handleChange}/>
+                              <label for="subject">Subject</label>
+                            </div>
+                          </div>
+                          <div className="col-md-12">
+                            <div className="md-form">
+                              <textarea className="md-textarea" id="message" name="message" required="required" onChange={this.handleChange}></textarea>
+                              <label for="message">Your message</label>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-md-12">
-                          <div className="md-form">
-                            <input className="form-control" id="subject" type="text" name="subject" required="required"/>
-                            <label for="subject">Subject</label>
-                          </div>
+                        <div className="center-on-small-only mb-4">
+                          <button className="btn btn-indigo ml-0" type="submit"><i className="fa fa-paper-plane-o mr-2"></i> Send</button>
                         </div>
-                        <div className="col-md-12">
-                          <div className="md-form">
-                            <textarea className="md-textarea" id="message" name="message" required="required"></textarea>
-                            <label for="message">Your message</label>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="center-on-small-only mb-4">
-                        <button className="btn btn-indigo ml-0" type="submit"><i className="fa fa-paper-plane-o mr-2"></i> Send</button>
-                      </div>
-                    </form>
-                  </div>
-                  <div className="col-md-4">
-                    <ul className="list-unstyled text-center">
-                      <li className="mt-4"><i className="fa fa-map-marker indigo-text fa-2x"></i>
-                        <p className="mt-2">Osaka, Japan</p>
-                      </li>
-                      <a href="https://twitter.com/_nobiki" target="_blank">
-                      <li className="mt-4"><i className="fa fa-twitter indigo-text fa-2x"></i>
-                        <p className="mt-2">@_nobiki</p>
-                      </li>
-                      </a>
-                    </ul>
+                      </form>
+                    </div>
+                    <div className="col-md-4">
+                      <ul className="list-unstyled text-center">
+                        <li className="mt-4"><i className="fa fa-map-marker indigo-text fa-2x"></i>
+                          <p className="mt-2">Osaka, Japan</p>
+                        </li>
+                        <a href="https://twitter.com/_nobiki" target="_blank">
+                        <li className="mt-4"><i className="fa fa-twitter indigo-text fa-2x"></i>
+                          <p className="mt-2">@_nobiki</p>
+                        </li>
+                        </a>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </section>
